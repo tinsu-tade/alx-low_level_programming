22c40a3449f9b142680bdb65de558bf25b82a0fc
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-06-17 23:47:55 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int main(void) /* description here */
+int main(void) \- Betty documentation checker for C source code
 {
 char letter[] = "\"Programming is like building a multilingual puzzle";
 puts(letter);
