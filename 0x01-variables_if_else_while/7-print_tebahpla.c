23d87577f7852d9a90b6e951c39f0f41d1700c84
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-06-21 23:35:38 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-int main(void) 
+int main(void)
 {
-   
-   char ch;
 
-   for (ch = 'z'; ch >= 'a'; ch--)
-   putchar(ch);
-   putchar('\n');
-   return (0);
+char ch;
+
+for (ch = 'z'; ch >= 'a'; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
 }
