cab8c1e2b200904653b151eb958b96c2c3caed30
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-06-21 23:28:31 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
-int main(void) 
+int main(void)
 {
-   char ch;
-  
-   for (ch = 'a'; ch <= 'z'; ch++)
-   putchar(ch);
-   for (ch = 'A'; ch <= 'Z'; ch++)
-   putchar(ch);
-   putchar('\n');
-   return (0);
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
 }
