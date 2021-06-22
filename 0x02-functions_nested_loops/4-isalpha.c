1e81f231741d0774c561c97dd1cf4850374b439c
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-06-22 21:35:05 +0300

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "holberton.h"
+
+int _isalpha(int c);
+int _isalpha(int c)
+{
+    if(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+        return 1;
+        else
+        return 0;
+}
