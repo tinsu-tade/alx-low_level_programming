c2f10b38f98e67115adb68c3a589fc437d9eab29
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-06-22 15:24:21 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include "holberton.h"
+/**
+* main - Entry point
+*
+* Return: Success (0)
+*/
 
 void print_alphabet(void)
 {
 char ch = 'a';
+  
 while(ch < 'z')
 {
 putchar(ch);
