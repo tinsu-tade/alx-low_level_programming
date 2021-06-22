2255d781d7dad431a946e70f5776660a9f8fe4fd
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-06-22 14:10:48 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int main(void)
 {
 int i = 0;
