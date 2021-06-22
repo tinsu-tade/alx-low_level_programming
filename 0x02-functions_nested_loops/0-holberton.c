4ca425c266182ad8a2ba9cdeeebed92530c4f412
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-06-22 14:26:34 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i = 0;
 char a[] = "Holberton";
-     
+
 while (i < 9)
 {
 putchar(a[i]);
