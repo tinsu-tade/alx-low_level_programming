96d5a814e90f346fcbc79b3845ea3f49e3d08280
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-06-22 14:35:16 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+void printAlpha();
+int main(void)
+{
+printAlpha();
+return (0);
+}
 
+void printAlpha()
+{
+char ch = 'a';
+while(ch < 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+}
