5ea8835edec5502d0f53d93f2c22a5897bd3ceca
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-11-04 11:18:01 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+#include "holberton.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: pointer with the address of the string
+ *
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	int count = 0;
 
+	if (s[count] != '\0')
+	{
+		_putchar(s[count]);
+		count++;
+		_void _print_rev_recursion(char *s);(&s[count]);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
