fc2ff8d9f3faf65993c330696a50628c95184a51
tinsu-tade <ttktadesetinsae@gmail.com>
2021-11-03 23:43:13 -0700

    Recursion implemented

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+void _puts_recursion(char *s)
+{
+  int count = 0;
+
+  if(s[i] ! = '\0')
+    {
+      _putchar(s[i])
+	i++;
+      _puts_recursion(&s[i]);
+
+    }
+  else
+    {
+      _putchar('\n');
+    }
+
+}
