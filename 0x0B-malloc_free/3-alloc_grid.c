b49c1d1b8446105d94cedb744ea886b15ef3847b
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-11-09 16:41:58 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,48 +1,44 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include "holberton.h"
+#include <stdio.h>
+
+
 /**
- *alloc_grid - returns a pointer to a 2 dimensional array of characters
- *@width:w
- *@height:l
- *
- *Return:NULL, Pointer
- *
- */
+ * alloc_grid - function that returns a pointer to a 2 dimensional
+ * array of integers.
+ * @width: the length of the array
+ * @height: the height of the array
+ * Return: the two dimensional array
+ **/
+
+
 int **alloc_grid(int width, int height)
 {
-	int i = 0, j;
-	int **arr;
+	int **newarray;
+	int counter, counter2, k;
 
 	if (width <= 0 || height <= 0)
+	return (NULL);
+
+	newarray = malloc(height * sizeof(int *));
+
+	if (newarray == NULL)
+	return (NULL);
+
+	for (counter = 0; counter < height; counter++)
 	{
-		return (NULL);
-	}
-	arr = (int **)malloc(height * sizeof(int *));
-	if (!arr)
+	newarray[counter] = malloc(width * sizeof(int));
+
+	if (newarray[counter] == NULL)
 	{
-		free(arr);
-		return (NULL);
+	for (counter2 = 0 ; counter2 <= counter; counter2++)
+	free(newarray[counter2]);
+	free(newarray);
+	return (NULL);
 	}
-	for (i = 0; i < height; i++)
-	{
-		arr[i] = (int *)malloc(width * sizeof(int));
-		if (!arr[i])
-		{
-			for (j = 0; j <= i; j++)
-			{
-				free(arr[j]);
-			}
-			free(arr);
-			return (NULL);
-		}
+
+	for (k = 0; k < width; k++)
+	newarray[counter][k] = 0;
 	}
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-		{
-			arr[i][j] = 0;
-		}
-	}
-	return (arr);
+	return (newarray);
 }
