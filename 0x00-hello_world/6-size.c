97b18140ec312845c644907d13bd531003f0f212
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-11-15 10:25:57 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int l = sizeof(long int);
 int m = sizeof(long long int);
 int f = sizeof(float);
 
-printf("Size of a char: %d byte(s)\n", c);
-printf("Size of an int: %d byte(s)\n", i);
-printf("Size of a long int: %d byte(s)\n", l);
-printf("Size of a long int: %d byte(s)\n", m);
-printf("Size of a float: %d byte(s)\n", f);
+printf("Size of a char: %zu byte(s)\n", c);
+printf("Size of an int: %zu byte(s)\n", i);
+printf("Size of a long int: %zu byte(s)\n", l);
+printf("Size of a long int: %zi byte(s)\n", m);
+printf("Size of a float: %zu byte(s)\n", f);
 return (0);
 }
