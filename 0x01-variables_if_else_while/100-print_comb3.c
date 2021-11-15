f0167db8b3ac2b6bd20c1dc576779c1f2b0b5639
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-11-15 12:08:42 +0300

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+*main - Entry Point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+for(char i = 48; i <= 57; i++)
+{      
+for (char k = 48; k <= 57 ; k++)
+{
+if(i<57 || k<57)
+{
+putchar(i);
+putchar(k);
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar(i);
+putchar(k);         
+}   
+
+}	
+
+} 
+}
+
+
