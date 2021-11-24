35dbd3dfe626dbff94cdcd00ac41d69eecd47781
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-11-24 09:42:42 +0300

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * main - check the code
+ *@s: character pointer
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+int counter = 0;
+while(*s != '\0')
+{
+s++;
+counter++;
+}
+return (counter);
+}
