bea073755c7e8778f78ac6794323812d8ff2e830
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-11-24 11:09:50 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include<unistd>
+/**
+ * _puts - prints a char
+ *@str: character pointer
+ * Return: nothing.
+ */
+void _puts(char *str)
+{
+while(*str != '\0')
+{
+write(1,str,1);
+str++;
+} 
+}
