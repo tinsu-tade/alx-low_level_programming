a7fe06c41d23604cb965832b89320355ba27fcb9
Tinsae Tadese <74678754+tinsu-tade@users.noreply.github.com>
2021-11-30 09:19:58 +0300

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/*
+*size_t - prints members of the list
+*@listint_t: a pointer to the head of the linked list 
+*
+*Return: returns the number of nodes on linked list
+*/
+size_t print_listint(const listint_t *h)
+{
+  int num_nodes = 0;
+  while(h != NULL)
+  {
+    printf("%d\n",h->n);
+    h = h->next;
+    num_nodes++;
+  }
+  return (num_nodes);
+}
