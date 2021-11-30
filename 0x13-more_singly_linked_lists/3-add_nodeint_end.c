83962bd17e908a55d15a312cf033ae5f2b09256f
Tinsae Tadese <74678754+tinsu-tade@users.noreply.github.com>
2021-11-30 14:39:41 +0300

    Create 3-add_nodeint_end.c

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,31 @@
+/**
+ *
+ *listint_t - add node at the end.
+ *@head: a pointer points to pointer that points to node head.
+ *@n: holds an integer data.
+ *Return: address of a new node.
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *lastnode;
+listint_t *prevnode;
+lastnode = malloc(sizeof(listint_t));
+
+if(lastnode == NULL)
+return (NULL);	   
+lastnode->n = n;
+lastnode->next = NULL;
+
+if(*head == NULL)
+{
+*head = lastnode;
+return (lastnode);
+}
+prevnode = *head;
+
+while (prevnode->next)
+prevnode = prevnode->next;
+
+prevnode->next = lastnode;  	  
+}
