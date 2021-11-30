00e78eb38b94dc77fe7b29e821f0acc7d3a550af
Tinsae Tadese <74678754+tinsu-tade@users.noreply.github.com>
2021-11-30 09:39:55 +0300

    Create 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *listint_len - counts numbers of node in the linked list.
+ *@h: a pointer to the head of the list.
+ *
+ *Returns: number of node in the list 
+ */
+
+size_t listint_len(const listint_t *h)
+{
+int num_nodes = 0;
+while(h != NULL)
+{
+
+h = h->next;
+num_nodes++;
+}
+return (num_nodes);
+}
