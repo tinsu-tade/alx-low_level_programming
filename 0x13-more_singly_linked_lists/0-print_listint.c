143dc61ae3433835455730f3f132df606cba8437
Tinsae Tadese <74678754+tinsu-tade@users.noreply.github.com>
2021-11-30 09:31:26 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
-/*
-*size_t - prints members of the list
-*@listint_t: a pointer to the head of the linked list 
-*
-*Return: returns the number of nodes on linked list
-*/
+/**
+ *size_t - prints members of the list
+ *@listint_t: a pointer to the head of the linked list 
+ *
+ *Return: returns the number of nodes on linked list
+ */
 size_t print_listint(const listint_t *h)
 {
   int num_nodes = 0;
